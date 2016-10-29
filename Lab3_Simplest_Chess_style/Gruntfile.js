7602c7b2d542c3b1febdbfb1b79cfc3e5a5437d2
FX <marian.focsa@outlook.com>
2016-10-29 16:15:40 +0300

    Grunt + Browserify Setup

--- Lab3_Simplest_Chess_style/Gruntfile.js
+++ Lab3_Simplest_Chess_style/Gruntfile.js
@@ -1,5 +1,14 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      'public/app.js': ['client/app.js']
+    }
+    watch: {
+      files: [ "client/**/*.js"],
+      tasks: [ 'browserify' ]
+    }
   })
+  grunt.loadNpmTasks('grunt-browserify')
+  grunt.loadNpmTasks('grunt-contrib-watch')
 }
